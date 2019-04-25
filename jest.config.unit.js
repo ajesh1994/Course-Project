5ce14129fa35b2183d7b2d52b0ae5f9a8b9b022a
Ajesh Jally <ajeshjally@gmail.com>
2019-04-25 10:29:38 +0100

    iiii

--- jest.config.unit.js
+++ jest.config.unit.js
@@ -11,5 +11,5 @@ module.exports = {
   testMatch: [
     "<rootDir>/(tests/unit/**/*.spec.(js|jsx|ts|tsx)|**/__tests__/*.(js|jsx|ts|tsx))"
   ],
-  transformIgnorePatterns: ["<rootDir>/node_modules/"]
+  transformIgnorePatterns: ["node_modules/(?!(react-redux|fetch-mock)/)"]
 };
